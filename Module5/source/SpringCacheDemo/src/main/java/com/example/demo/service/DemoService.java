1664e0e8233f41acb34fceda5eafc5110b2cb754
TekCreek <55848762+TekCreek@users.noreply.github.com>
2026-03-26 22:32:42 +0530

    Added loggers

--- Module5/source/SpringCacheDemo/src/main/java/com/example/demo/service/DemoService.java
+++ Module5/source/SpringCacheDemo/src/main/java/com/example/demo/service/DemoService.java
@@ -1,17 +1,25 @@
 package com.example.demo.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DemoService {
 
+    private Logger logger = LoggerFactory.getLogger(DemoService.class);
+
     @Cacheable(value = "answerCache", key = "#questionNumber")
     public String getAnswer(int questionNumber) {
+
+        logger.info("Fetching the question from DB");
 
         // Simulate delay
         try { Thread.sleep(5000); } catch(InterruptedException e) {}
 
-        return "Answer for questionNumber : " + questionNumber;
+        logger.info("Sending the response ");
+
+        return "Answer for questionNumber : " + questionNumber + " is `I don't know` ";
     }
 }
